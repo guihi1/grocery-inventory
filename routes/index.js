6ffde0a5725f9c6de4f98f5c025c7f6693cc2eba
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-02-15 16:27:38 -0300

    Use indexController

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
+const indexController = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Inventory Home' });
-});
+router.get('/', indexController.index);
 
 module.exports = router;
