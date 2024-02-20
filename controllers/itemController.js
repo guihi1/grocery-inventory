d0076e609e00a19674e6b3bcc54e5fc0784f094e
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-02-20 17:02:30 -0300

    Add create item form and handle post request

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 const Item = require('../model/item');
+const Category = require('../model/category');
 
 // Display list of all items.
 exports.item_list = asyncHandler(async (req, res, next) => {
@@ -15,13 +17,50 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 
 // Display item create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: item create GET');
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+  res.render('item_form', { title: 'Add Item', categories: allCategories });
 });
 
 // Handle item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: item create POST');
-});
+exports.item_create_post = [
+  // Validation
+  body('name', 'Item name must have at least 2 characters.')
+    .trim()
+    .isLength({ min: 2 })
+    .escape(),
+  body('description', 'Description must have at least 5 characters.')
+    .trim()
+    .isLength({ min: 5 })
+    .escape(),
+  body('price', 'Invalid price.').trim().isNumeric(),
+  body('category', 'Category must not be empty.').trim().notEmpty().escape(),
+  body('stock', 'Must be a number.').trim().isNumeric(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      stock: req.body.stock,
+    });
+
+    // When there are errors, render form again with error messages and previous values
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+      res.render('item_form', {
+        title: 'Add Item',
+        item,
+        categories: allCategories,
+        errors: errors.array(),
+      });
+    } else {
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 
 // Display item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
