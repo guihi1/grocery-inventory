ba41b9f29dfc213fc744364d63e52f4053f924ac
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-02-21 16:45:20 -0300

    Handle update get and post requests

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -76,10 +76,55 @@ exports.item_delete_post = [
 
 // Display item update form on GET.
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: item update GET');
+  const [item, allCategories] = await Promise.all([
+    Item.findById(req.params.id).populate('category').exec(),
+    Category.find().sort({ name: 1 }).exec(),
+  ]);
+  res.render('item_form', {
+    title: 'Update Item',
+    item,
+    categories: allCategories,
+  });
 });
 
 // Handle item update on POST.
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: item update POST');
-});
+exports.item_update_post = [
+  // Validation
+  body('name', 'Item name must have at least 2 characters.')
+    .trim()
+    .isLength({ min: 2 })
+    .escape(),
+  body('description', 'Description must have at least 5 characters.')
+    .trim()
+    .isLength({ min: 5 })
+    .escape(),
+  body('price', 'Invalid price.').trim().isNumeric(),
+  body('category', 'Category must not be empty.').trim().notEmpty().escape(),
+  body('stock', 'Must be a number.').trim().isNumeric(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      stock: req.body.stock,
+      _id: req.params.id,
+    });
+
+    // When there are errors, render form again with error messages and previous values
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+      res.render('item_form', {
+        title: 'Update Item',
+        item,
+        categories: allCategories,
+        errors: errors.array(),
+      });
+    } else {
+      const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {});
+      res.redirect(updatedItem.url);
+    }
+  }),
+];
