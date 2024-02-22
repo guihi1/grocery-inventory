d10e2055d29ada5f87a26534ea532caa92f629f3
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-02-22 15:55:59 -0300

    Changes for production

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 
 // Import routers
 const indexRouter = require('./routes/index');
@@ -12,6 +15,13 @@ const categoryRouter = require('./routes/categoryRoute');
 const itemRouter = require('./routes/itemRoute');
 
 const app = express();
+
+// Allow maximum of 100 requests per minute
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+  message: 'You have exceeded 100 requests in one minute!',
+});
 
 // Set up mongoose connection
 mongoose.set('strictQuery', false);
@@ -26,6 +36,9 @@ main().catch((err) => console.log(err));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(limiter);
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
